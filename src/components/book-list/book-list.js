5eb19198b695ad341fc7b40ea951fd813abf7ce2
Fiodor Furick <fff24.06.91@gmail.com>
2021-01-29 10:48:49 +0200

    add BookListContainer

--- src/components/book-list/book-list.js
+++ src/components/book-list/book-list.js
@@ -8,7 +8,19 @@ import compose from "../../utils";
 import Spinner from "../spinner";
 import ErrorIndicator from "../error-indicator";
 
-class BookList extends Component {
+const BookList = ({books}) => {
+    return (
+        <ul className={'book-list'}>
+            {
+                books.map((book) => {
+                    return <li key={book.id}><BookListItem book={book}/></li>
+                })
+            }
+        </ul>
+    )
+}
+
+class BookListContainer extends Component {
 
     componentDidMount() {
         this.props.fetchBooks();
@@ -26,13 +38,7 @@ class BookList extends Component {
         }
 
         return (
-            <ul className={'book-list'}>
-                {
-                    books.map((book) => {
-                        return <li key={book.id}><BookListItem book={book}/></li>
-                    })
-                }
-            </ul>
+            <BookList books={books}/>
         )
     }
 }
@@ -43,10 +49,10 @@ const mapStateToProps = ({books, loading, error}) => {
 
 const mapDispatchToProps = (dispatch, {bookstoreService}) => {
     return {
-        fetchBooks: fetchBooks (bookstoreService,dispatch)
+        fetchBooks: fetchBooks(bookstoreService, dispatch)
     }
 }
 
 export default compose(
     withBookstoreService(), connect(mapStateToProps, mapDispatchToProps)
-)(BookList);
+)(BookListContainer);
